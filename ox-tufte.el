0d90fc0f4acad1fc7767f7686a93f6df4ad05006
Suhail Singh <425284-suhailsingh@users.noreply.gitlab.com>
2024-01-12 12:03:18 -0500

    version: 3.0.4 => 4.0.0

--- ox-tufte.el
+++ ox-tufte.el
@@ -8,7 +8,7 @@
 ;; Maintainer: The Bayesians Inc.
 ;; Description: An org exporter for Tufte HTML
 ;; Keywords: org, tufte, html, outlines, hypermedia, calendar, wp
-;; Version: 3.0.4
+;; Version: 4.0.0
 ;; Package-Requires: ((org "9.5") (emacs "27.1"))
 ;; URL: https://github.com/ox-tufte/ox-tufte
 
