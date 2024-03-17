ebdde02e4d33c3321543d67db8f1aef80adc03bd
Suhail <suhail@bayesians.ca>
2024-03-17 16:53:21 -0400

    version: 4.1.1 => 4.2.0

--- ox-tufte.el
+++ ox-tufte.el
@@ -8,7 +8,7 @@
 ;; Maintainer: The Bayesians Inc.
 ;; Description: An org exporter for Tufte HTML
 ;; Keywords: org, tufte, html, outlines, hypermedia, calendar, wp
-;; Version: 4.1.1
+;; Version: 4.2.0
 ;; Package-Requires: ((org "9.5") (emacs "27.1"))
 ;; URL: https://github.com/ox-tufte/ox-tufte
 
